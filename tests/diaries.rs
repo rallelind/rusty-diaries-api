880319c2b230a67c506b92c5ed012d9d86fe94a5
Rasmus Lind <rasmus1234555@hotmail.dk>
2022-11-20 17:50:06 +0100

    added more test cases for getting a diary endpoint

--- tests/diaries.rs
+++ tests/diaries.rs
@@ -1,11 +1,15 @@
 
+use mongodb::bson::DateTime;
+use mongodb::results::InsertOneResult;
+use rocket::local::asynchronous::LocalResponse;
+use rocket::serde::json::Json;
 use test_modules::initiate_api::initiate;
 use test_modules::models::diary_model::Diary;
-
-use test_modules::models::encrypt::Encryption;
+use rocket::serde::Deserialize;
 
 use rocket::local::blocking::Client;
 use rocket::http::{Status, ContentType};
+use test_modules::models::encrypt::Encryption;
 
 fn initiate_rocket_client() -> Client {
 
@@ -24,25 +28,29 @@ fn test_get_diary() {
     assert_eq!(good_response.status(), Status::Ok);
     assert_eq!(bad_response.status(), Status::BadRequest);
     assert_eq!(not_found_document.status(), Status::new(404));
+
+    let good_response_body = good_response.into_json::<Diary>().unwrap();
+
+    let Diary { title, description, updated_at, date, .. } = good_response_body;
+
+    assert_eq!(title, Encryption::decrypt("s+ZhSR4QBbxuN7Ys+mcy8w==".to_string()));
+    assert_eq!(description, Encryption::decrypt("b28Ru7ppUm0USSqWpOhzl5S7kzh8o7ODXFeuY68K/JQ=".to_string()));
+    assert_eq!(date.unwrap().to_string(), "2022-11-16 22:24:55.467 +00:00:00".to_string());
+    assert_eq!(updated_at, None);
 }
 
 #[test]
 fn test_post_diary() {
-    
 
     let client = initiate_rocket_client();
 
     let response = client.post("/api/diary")
-        .header(ContentType::JSON)
         .body(r##"{
             "title": "post title",
             "description": "test post description"
         }"##)
         .dispatch();
 
-    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.status(), Status::Ok);    
 
-    let response_body = response.into_json::<Diary>();
-
-    assert_eq!(response_body.unwrap().title, "post title");
 }
